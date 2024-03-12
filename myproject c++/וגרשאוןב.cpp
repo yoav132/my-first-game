10647c49bbf559e573963598ee2adfc3306a26f5
Yoav Messika <157037968+yoav132@users.noreply.github.com>
2024-03-12 13:36:54 +0200

    Update וגרשאוןב.cpp

--- myproject c++/וגרשאוןב.cpp
+++ myproject c++/וגרשאוןב.cpp
@@ -33,11 +33,11 @@ int GetRandomNumber(int min, int max)
 double pi = 3.1415926535897932;
 const int VIEW_RANGE = 20;
 const int VIEW_RANGE_FIXER = VIEW_RANGE/2;
-const int MAP_WIDTH = 20;
-const int MAP_HEIGHT = 10;
+const int MAP_WIDTH = 40;
+const int MAP_HEIGHT = 35;
 char GameMap[MAP_HEIGHT][MAP_WIDTH];
-int player_X= MAP_HEIGHT/2;
-int player_Y= MAP_WIDTH/2;
+int player_X= VIEW_RANGE_FIXER/2;
+int player_Y= VIEW_RANGE/2;
 int randomNumber;
 int item1_Y;
 int item1_X;
@@ -89,17 +89,27 @@ void Map()
     }
 }
 
+int i = player_Y - MAP_WIDTH / 2;
+int j = player_X - MAP_HEIGHT / 2;
+int temp_i=i;
+int temp_j=j;
 
 
 void DrawMap()
 {
-    for (size_t i = player_Y - VIEW_RANGE / 2; i < player_Y + VIEW_RANGE / 2; i++)
+    i = temp_i;
+    for (; i < player_Y + VIEW_RANGE_FIXER / 2 + temp_i; i++)
     {
-        for (size_t j = player_X - VIEW_RANGE_FIXER / 2; j < player_X + VIEW_RANGE_FIXER / 2; j++)
+        j = temp_j;
+        for (; j < player_X + VIEW_RANGE / 2 + temp_j; j++)
         {
-            if (j > -1 && i > -1 && j < MAP_HEIGHT-1 && i < MAP_WIDTH-1)
+            if (j > -1 && i > -1 && j < MAP_HEIGHT - 1 && i < MAP_WIDTH - 1)
             {
                 std::cout << GameMap[i][j];
+            }
+            else
+            {
+                std::cout << GO_WALL;
             }
         }
         std::cout << "\n";
@@ -112,19 +122,22 @@ void keypress()
     keypressed = _getch();
     if (keypressed == 'w' && player_X!=1)
     {
-        player_X--;
+        if (temp_i > MAP_HEIGHT)
+        {
+            temp_i--;
+        }
     }
     else if(keypressed=='d' && player_Y < MAP_WIDTH-2)
     {
-        player_Y++;
+        temp_j++;
     }
     else if (keypressed=='s' && player_X < MAP_HEIGHT-2)
     {
-       player_X++;
+       temp_i++;
     }
     else if (keypressed == 'a' && player_Y != 1)
     {
-        player_Y--;
+       temp_j--;
     }
 }
 
